59e2636408e1e70d5f6bb4094c9adeb7d6376c47
Dick Visser <dick.visser@geant.org>
2025-06-18 14:38:44 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
 # nagios-check-email-round-trip
-Nagios plugin to monitor email round trips
+
+Nagios plugin to monitor the email round trip status. It will create and send an
+email message to an email address, and check an IMAP mailbox for the message to
+arrive.
+
+* `OK` when the message arrives within 10 minutes in the `INBOX`
+* `WARNING` when the message arives within 10 minutes in the `Spam` folder
+* `CRITICAL` when the message does not arrive with 10 minutes at all
+
+# Requirements
+
+* An SMTP relay server to send mail through
+* An IMAP account that will receive the message
+
+# Usage
+
+```
+usage: check-email-round-trip.py [-h] --smtp-server SMTP_SERVER [--smtp-port SMTP_PORT]
+                                 [--smtp-username SMTP_USERNAME] [--smtp-password SMTP_PASSWORD]
+                                 --smtp-from SMTP_FROM --smtp-to SMTP_TO [--subject-prefix SUBJECT_PREFIX]
+                                 [--body BODY] [--smtp-debuglevel SMTP_DEBUGLEVEL] --imap-server IMAP_SERVER
+                                 [--imap-port IMAP_PORT] --imap-username IMAP_USERNAME --imap-password IMAP_PASSWORD
+                                 [--imap-inbox-folder IMAP_INBOX_FOLDER] [--imap-spam-folder IMAP_SPAM_FOLDER]
+                                 [--imap-poll-interval IMAP_POLL_INTERVAL] [--max-wait MAX_WAIT] [-v]
+
+A Nagios script to monitor an email round trip
+
+options:
+  -h, --help            show this help message and exit
+  --smtp-server SMTP_SERVER
+                        SMTP server address (default: None)
+  --smtp-port SMTP_PORT
+                        SMTP server port (default: 587)
+  --smtp-username SMTP_USERNAME
+                        SMTP username (default: None)
+  --smtp-password SMTP_PASSWORD
+                        SMTP password (default: None)
+  --smtp-from SMTP_FROM
+                        FROM address to use for sending (default: None)
+  --smtp-to SMTP_TO     TO address to use for sending (default: None)
+  --subject-prefix SUBJECT_PREFIX
+                        Optional prefix for the message's Subject header. (default: Email monitoring )
+  --body BODY           Body of the message (default: Nagios email round trip test. This message can be ignored)
+  --smtp-debuglevel SMTP_DEBUGLEVEL
+                        SMTP debug level (default: 0)
+  --imap-server IMAP_SERVER
+                        IMAP server address (default: None)
+  --imap-port IMAP_PORT
+                        IMAP server port (default: 993)
+  --imap-username IMAP_USERNAME
+                        IMAP username (default: None)
+  --imap-password IMAP_PASSWORD
+                        IMAP password (default: None)
+  --imap-inbox-folder IMAP_INBOX_FOLDER
+                        IMAP INBOX folder (default: INBOX)
+  --imap-spam-folder IMAP_SPAM_FOLDER
+                        IMAP Spam folder (default: Spam)
+  --imap-poll-interval IMAP_POLL_INTERVAL
+                        IMAP polling interval (seconds) (default: 5)
+  --max-wait MAX_WAIT   Maximum time (seconds) to wait for message (default: 600)
+  -v, --verbosity       increase output verbosity (default: None)
+```
+
+# Caveats
+
+* If you use a GMail IMAP account, the Spam folder name should be configured
+  like this, including the double quotes:
+
+  ```
+  --imap-spam-folder '"[Gmail]/Spam"'
+  ```
+
+# To do
+
+* Allow reading credentials from files instead of command line arguments
---
+++ check-email-round-trip.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+import argparse
+import datetime
+import imaplib
+import smtplib
+import sys
+import time
+import uuid
+from email.message import EmailMessage
+from email.utils import make_msgid
+
+ap = argparse.ArgumentParser(
+    description="A Nagios script to monitor an email round trip",
+    # epilog="BUGS: no known bugs",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+
+# Arg names with dashes will become variables with underscores
+ap.add_argument("--smtp-server", help="SMTP server address", required=True)
+ap.add_argument("--smtp-port", help="SMTP server port", default=587)
+ap.add_argument("--smtp-username", help="SMTP username")
+ap.add_argument("--smtp-password", help="SMTP password")
+ap.add_argument("--smtp-from", help="FROM address to use for sending", required=True)
+ap.add_argument("--smtp-to", help="TO address to use for sending", required=True)
+ap.add_argument(
+    "--subject-prefix",
+    help="Optional prefix for the message's Subject header.",
+    default="Email monitoring ",
+)
+ap.add_argument(
+    "--body",
+    help="Body of the message",
+    default="Nagios email round trip test. This message can be ignored",
+)
+ap.add_argument("--smtp-debuglevel", help="SMTP debug level", default=0)
+ap.add_argument("--imap-server", help="IMAP server address", required=True)
+ap.add_argument("--imap-port", help="IMAP server port", default=993)
+ap.add_argument("--imap-username", help="IMAP username", required=True)
+ap.add_argument("--imap-password", help="IMAP password", required=True)
+ap.add_argument("--imap-inbox-folder", help="IMAP INBOX folder", default="INBOX")
+ap.add_argument("--imap-spam-folder", help="IMAP Spam folder", default="Spam")
+ap.add_argument(
+    "--imap-poll-interval", help="IMAP polling interval (seconds)", default=5
+)
+ap.add_argument(
+    "--max-wait", help="Maximum time (seconds) to wait for message", default=600
+)
+ap.add_argument("-v", "--verbosity", action="count", help="increase output verbosity")
+args = ap.parse_args()
+
+SMTP_SERVER = args.smtp_server
+SMTP_PORT = args.smtp_port
+SMTP_USERNAME = args.smtp_username
+SMTP_PASSWORD = args.smtp_password
+FROM_EMAIL = args.smtp_from
+TO_EMAIL = args.smtp_to
+SUBJECT = args.subject_prefix + str(uuid.uuid4())
+BODY = args.body
+SMTP_DEBUGLEVEL = args.smtp_debuglevel
+
+IMAP_SERVER = args.imap_server
+IMAP_PORT = args.imap_port
+IMAP_USERNAME = args.imap_username
+IMAP_PASSWORD = args.imap_password
+IMAP_FOLDER_INBOX = args.imap_inbox_folder
+#  IMAP_FOLDER_SPAM = '"[Gmail]/Spam"'
+IMAP_FOLDER_SPAM = args.imap_spam_folder
+POLL_INTERVAL = int(args.imap_poll_interval)
+MAX_WAIT_SECONDS = args.max_wait
+VERBOSITY = args.verbosity
+
+
+def send_email():
+    try:
+        msg = EmailMessage()
+        msg["Message-ID"] = make_msgid()
+        msg["Subject"] = SUBJECT
+        msg["From"] = FROM_EMAIL
+        msg["To"] = TO_EMAIL
+        msg.set_content(BODY)
+
+        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+            server.set_debuglevel(SMTP_DEBUGLEVEL)
+            server.starttls()
+            if None not in (SMTP_USERNAME, SMTP_PASSWORD):
+                server.login(SMTP_USERNAME, SMTP_PASSWORD)
+            server.mail(FROM_EMAIL)
+            server.rcpt(TO_EMAIL)
+            # sendmail() or send_message() does not return code/response,
+            # so we resort to data()
+            response_code, response_string = server.data(msg.as_string())
+            server.quit()
+            return response_code, response_string.decode()
+    except Exception as e:
+        print(f"UNKNOWN - an error occurred: {e}")
+        sys.exit(3)
+
+
+def check_imap_message(folders):
+    try:
+        mail = imaplib.IMAP4_SSL(IMAP_SERVER, IMAP_PORT)
+        mail.login(IMAP_USERNAME, IMAP_PASSWORD)
+        # mail.debug = 4
+        for _ in range(0, MAX_WAIT_SECONDS, POLL_INTERVAL):
+            for folder in folders:
+                if VERBOSITY is not None:
+                    print(f"Checking for message in folder {folder}")
+                mail.select(folder)
+                result, data = mail.search(
+                    None, f'(SUBJECT "{SUBJECT}" HEADER FROM "{FROM_EMAIL}")'
+                )
+                if result == "OK":
+                    email_ids = data[0].split()
+                    for email_id in email_ids:
+                        result, msg_data = mail.fetch(email_id, "(RFC822)")
+                        if result == "OK":
+                            if VERBOSITY is not None:
+                                print(f"Found message in folder {folder}")
+                            # A tuple of the message, and the folder it was in
+                            return msg_data[0][1].decode(), folder
+            time.sleep(POLL_INTERVAL)
+        return None, None
+    except Exception as e:
+        print(f"UNKNOWN - an error occurred: {e}")
+        sys.exit(3)
+
+
+def main():
+    try:
+        smtp_code, smtp_response = send_email()
+        sent_at = datetime.datetime.now()
+        smtp_log = (
+            f"SMTP server {SMTP_SERVER}:{SMTP_PORT} said: {smtp_code} {smtp_response}"
+        )
+    except Exception as e:
+        print(f"CRITICAL - Failed to send email: {e}")
+        sys.exit(2)
+
+    # Try to fetch the message
+    found_message, folder = check_imap_message([IMAP_FOLDER_INBOX, IMAP_FOLDER_SPAM])
+
+    if found_message is not None:
+        received_at = datetime.datetime.now()
+        round_trip_time = round((received_at - sent_at).total_seconds())
+        imap_log = f"IMAP server {IMAP_SERVER}:{IMAP_PORT} contains message in {folder}:\n\n{found_message}"
+        perf_data = f"rtt={round_trip_time}"
+        result = f"Email arrived in {folder}, message round trip took {round_trip_time} seconds | {perf_data}\n{smtp_log}\n{imap_log}"
+
+        if folder == IMAP_FOLDER_INBOX:
+            print(f"OK - {result}")
+            sys.exit(0)
+        if folder == IMAP_FOLDER_SPAM:
+            print(f"WARNING - {result}")
+            sys.exit(1)
+    else:
+        print(
+            f"CRITICAL - Email message not received within timeout of {MAX_WAIT_SECONDS} seconds\n{smtp_log}"
+        )
+        sys.exit(2)
+
+
+if __name__ == "__main__":
+    main()
